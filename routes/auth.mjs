556b86b14c411a4a61e1997a8a4a8c6afb8b192b
Valentin Negru <negru.valentin1795@gmail.com>
2021-07-25 13:43:37 +0000

    Add user registration

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { loginController, verifyTokenController } from '../controllers/auth.mjs';
+import { loginController, registerController, verifyTokenController } from '../controllers/auth.mjs';
 
 const router = express.Router();
 
 router.post('/login', loginController);
+
+router.post('/register', registerController);
 
 router.post('/token/:accessToken', verifyTokenController);
 
