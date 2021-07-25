eed745bc175b4a04a43af00f3364d0ae6c4645d3
Robert Spinean <robispinean@yahoo.com>
2021-07-25 16:00:59 +0200

    Add data for testing

---
+++ data/commentData.mjs
@@ -0,0 +1,13 @@
+const comments = [
+    {
+        description:"Comment1"
+    },
+    {
+        description:"Comment2"
+    },
+    {
+        description:"Comment3"
+    }
+]
+
+export default comments
---
+++ data/postData.mjs
@@ -0,0 +1,20 @@
+const posts = [
+    {
+        type: 'offer',
+        title: 'Title',
+        description: 'Description',
+        languages: ['Language1', 'Language2'],
+        workHour: 'Full time',
+        workPlace: 'Remote'
+    },
+    {
+        type: 'request',
+        title: 'Title',
+        description: 'Description',
+        languages: ['Language1', 'Language2'],
+        workHour: 'Part time',
+        workPlace: 'Remote'
+    },
+]
+
+export default posts
