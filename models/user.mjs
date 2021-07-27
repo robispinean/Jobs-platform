1818cba292a20208528cd58e685cc219d3f97d41
Robert Spinean <robispinean@yahoo.com>
2021-07-27 12:19:55 +0200

    Added encryption

--- models/user.mjs
+++ models/user.mjs
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -15,6 +16,15 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 }, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema, 'users');
 
